179c68e388722580ba31e47bcd3dd40610112788
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2026-01-16 16:55:54 -0500

    update: handbell pitch

--- common/src/main/java/com/chefmooon/colourfulclocks/common/data/types/HandbellTypes.java
+++ common/src/main/java/com/chefmooon/colourfulclocks/common/data/types/HandbellTypes.java
@@ -14,24 +14,24 @@ import java.util.function.Supplier;
 
 public enum HandbellTypes implements StringRepresentable {
 
-    IRON(0, "iron", Items.IRON_INGOT, ColourfulClocksSounds.ITEM_IRON_HANDBELL_RING, ColourfulClocksSounds.ITEM_IRON_HANDBELL_HIT, 1.5F),
-    COPPER(1, "copper", Items.COPPER_INGOT, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.5F),
-    EXPOSED_COPPER(2, "exposed_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.5F),
-    WEATHERED_COPPER(3, "weathered_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.5F),
-    OXIDIZED_COPPER(4, "oxidized_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.5F),
-    WAXED_COPPER(5, "waxed_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.5F),
-    WAXED_EXPOSED_COPPER(6, "waxed_exposed_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.5F),
-    WAXED_WEATHERED_COPPER(7, "waxed_weathered_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.5F),
-    WAXED_OXIDIZED_COPPER(8, "waxed_oxidized_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.5F),
-    GOLD(9, "gold", Items.GOLD_INGOT, ColourfulClocksSounds.ITEM_GOLD_HANDBELL_RING, ColourfulClocksSounds.ITEM_GOLD_HANDBELL_HIT, 1.5F),
-    DIAMOND(10, "diamond", Items.DIAMOND, ColourfulClocksSounds.ITEM_DIAMOND_HANDBELL_RING, ColourfulClocksSounds.ITEM_DIAMOND_HANDBELL_HIT, 1.5F),
-    NETHERITE(11, "netherite", Items.NETHERITE_INGOT, ColourfulClocksSounds.ITEM_NETHERITE_HANDBELL_RING, ColourfulClocksSounds.ITEM_NETHERITE_HANDBELL_HIT, 1.5F),
+    IRON(0, "iron", Items.IRON_INGOT, ColourfulClocksSounds.ITEM_IRON_HANDBELL_RING, ColourfulClocksSounds.ITEM_IRON_HANDBELL_HIT, 1.9F),
+    COPPER(1, "copper", Items.COPPER_INGOT, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.9F),
+    EXPOSED_COPPER(2, "exposed_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.9F),
+    WEATHERED_COPPER(3, "weathered_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.9F),
+    OXIDIZED_COPPER(4, "oxidized_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.9F),
+    WAXED_COPPER(5, "waxed_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.9F),
+    WAXED_EXPOSED_COPPER(6, "waxed_exposed_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.9F),
+    WAXED_WEATHERED_COPPER(7, "waxed_weathered_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.9F),
+    WAXED_OXIDIZED_COPPER(8, "waxed_oxidized_copper", Items.AIR, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_RING, ColourfulClocksSounds.ITEM_COPPER_HANDBELL_HIT, 1.9F),
+    GOLD(9, "gold", Items.GOLD_INGOT, ColourfulClocksSounds.ITEM_GOLD_HANDBELL_RING, ColourfulClocksSounds.ITEM_GOLD_HANDBELL_HIT, 1.6F),
+    DIAMOND(10, "diamond", Items.DIAMOND, ColourfulClocksSounds.ITEM_DIAMOND_HANDBELL_RING, ColourfulClocksSounds.ITEM_DIAMOND_HANDBELL_HIT, 1.4F),
+    NETHERITE(11, "netherite", Items.NETHERITE_INGOT, ColourfulClocksSounds.ITEM_NETHERITE_HANDBELL_RING, ColourfulClocksSounds.ITEM_NETHERITE_HANDBELL_HIT, 1.2F),
 
-    EMERALD(12, "emerald", Items.EMERALD, ColourfulClocksSounds.ITEM_EMERALD_HANDBELL_RING, ColourfulClocksSounds.ITEM_EMERALD_HANDBELL_HIT, 1.5F),
-    AMETHYST(13, "amethyst", Items.AMETHYST_SHARD, ColourfulClocksSounds.ITEM_AMETHYST_HANDBELL_RING, ColourfulClocksSounds.ITEM_AMETHYST_HANDBELL_HIT, 1.5F),
-    QUARTZ(14, "quartz", Items.QUARTZ, ColourfulClocksSounds.ITEM_QUARTZ_HANDBELL_RING, ColourfulClocksSounds.ITEM_QUARTZ_HANDBELL_HIT, 1.5F),
-    LAPIS_LAZULI(15, "lapis_lazuli", Items.LAPIS_LAZULI, ColourfulClocksSounds.ITEM_LAPIS_LAZULI_HANDBELL_RING, ColourfulClocksSounds.ITEM_LAPIS_LAZULI_HANDBELL_HIT, 1.5F),
-    REDSTONE(16, "redstone", Items.REDSTONE, ColourfulClocksSounds.ITEM_REDSTONE_HANDBELL_RING, ColourfulClocksSounds.ITEM_REDSTONE_HANDBELL_HIT, 1.5F)
+    EMERALD(12, "emerald", Items.EMERALD, ColourfulClocksSounds.ITEM_EMERALD_HANDBELL_RING, ColourfulClocksSounds.ITEM_EMERALD_HANDBELL_HIT, 1.6F),
+    AMETHYST(13, "amethyst", Items.AMETHYST_SHARD, ColourfulClocksSounds.ITEM_AMETHYST_HANDBELL_RING, ColourfulClocksSounds.ITEM_AMETHYST_HANDBELL_HIT, 1.6F),
+    QUARTZ(14, "quartz", Items.QUARTZ, ColourfulClocksSounds.ITEM_QUARTZ_HANDBELL_RING, ColourfulClocksSounds.ITEM_QUARTZ_HANDBELL_HIT, 1.6F),
+    LAPIS_LAZULI(15, "lapis_lazuli", Items.LAPIS_LAZULI, ColourfulClocksSounds.ITEM_LAPIS_LAZULI_HANDBELL_RING, ColourfulClocksSounds.ITEM_LAPIS_LAZULI_HANDBELL_HIT, 1.6F),
+    REDSTONE(16, "redstone", Items.REDSTONE, ColourfulClocksSounds.ITEM_REDSTONE_HANDBELL_RING, ColourfulClocksSounds.ITEM_REDSTONE_HANDBELL_HIT, 1.6F)
     ;
 
     public static final Codec<HandbellTypes> CODEC = StringRepresentable.fromEnum(HandbellTypes::values);
