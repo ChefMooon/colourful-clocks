b0960c158f125d9756261a0f4fd025d1f158c69b
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2026-01-21 12:03:29 -0500

    update: handbell item use
    - improve sound implementation
    - add cooldown
    - add item used stat

--- common/src/main/java/com/chefmooon/colourfulclocks/common/item/HandbellBlockItem.java
+++ common/src/main/java/com/chefmooon/colourfulclocks/common/item/HandbellBlockItem.java
@@ -6,13 +6,11 @@ import com.chefmooon.colourfulclocks.common.data.types.HandbellTypes;
 import com.chefmooon.colourfulclocks.common.registry.ColourfulClocksDataComponentTypes;
 import com.chefmooon.colourfulclocks.common.util.TextUtil;
 import net.minecraft.network.chat.Component;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 
@@ -46,7 +44,10 @@ public class HandbellBlockItem extends BlockItem {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
-        level.playLocalSound(player, type.getRingSound().get(), player.getSoundSource(), 1.0F, type.getPitch());
+        player.startUsingItem(usedHand);
+        level.playSound(player, player, type.getRingSound().get(), player.getSoundSource(), 1.0F, type.getPitch());
+        player.getCooldowns().addCooldown(this, 20); // TODO: review handbell ring cooldown. Seems responsible but maybe not fun.
+        player.awardStat(Stats.ITEM_USED.get(this));
         return InteractionResultHolder.consume(player.getItemInHand(usedHand));
     }
 }
